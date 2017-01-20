138d94c3b04688d6c7da775d56d86a8a1c1c7867
Jeison Villalobos <jeisonjlvb@gmail.com>
2017-01-20 10:18:25 -0400

    Probando header sin letras

--- index.php
+++ index.php
@@ -51,11 +51,11 @@
     <section id="main-slider" class="carousel">
         <div class="carousel-inner">
             <div class="item active">
-                <div class="container">
+                <div class="container"><!--
                     <div class="carousel-content">
                         <h1>Calidad de Servicios</h1>
                         <p class="lead">Todo lo que necesites al alcance de tus manos</p>
-                    </div>
+                    </div>-->
                 </div>
             </div><!--/.item-->
             <div class="item">
@@ -66,9 +66,9 @@
                     </div>
                 </div>
             </div><!--/.item-->
-        </div><!--/.carousel-inner-->
+        </div><!--/.carousel-inner--><!-- /////////iconos///////
         <a class="prev" href="#main-slider" data-slide="prev"><i class="icon-angle-left"></i></a>
-        <a class="next" href="#main-slider" data-slide="next"><i class="icon-angle-right"></i></a>
+        <a class="next" href="#main-slider" data-slide="next"><i class="icon-angle-right"></i></a> -->
     </section><!--/#main-slider-->
 
     <section id="services">
